fc72fdb9b104d7fae459bdfe3bbff2909b739689
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-06-08 07:17:02 +0700

    updates reports api with user relation and serialize the response

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,11 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from 'src/guards/auth.guard';
+import { User } from '../users/user.entity';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,7 +13,8 @@ export class ReportsController {
 
 	@Post()
 	@UseGuards(AuthGuard)
-	async createReport(@Body() request: CreateReportDto) {
-		return await this.reportsService.create(request);
+	@Serialize(ReportDto)
+	async createReport(@Body() request: CreateReportDto, @CurrentUser() user: User) {
+		return await this.reportsService.create(request, user);
 	}
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
@@ -11,9 +11,9 @@ export class ReportsService {
 		@InjectRepository(Report) private readonly reportRepository: Repository<Report>
 		) {}
 
-	async create(reportDto: CreateReportDto): Promise<Report | null> {
+	async create(reportDto: CreateReportDto, user: User): Promise<Report | null> {
 		const report = await this.reportRepository.create(reportDto);
-
+		report.user = user;
 		return this.reportRepository.save(report)
 	}
 }
